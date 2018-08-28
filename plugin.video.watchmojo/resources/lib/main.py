84042656c30101b62e65e6c2362725b3d1e3f9e6
willforde <willforde@gmail.com>
2018-08-28 02:55:10 +0100

    ignore unused run reference

--- plugin.video.watchmojo/resources/lib/main.py
+++ plugin.video.watchmojo/resources/lib/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
+
+# noinspection PyUnresolvedReferences
 from codequick import Route, Resolver, Listitem, utils, run
 import urlquick
 
